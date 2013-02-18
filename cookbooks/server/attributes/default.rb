23bc369b7e65acd887ac30052e3cdd0b30f5c38f
Varokas Panusuwan <varokas@panusuwan.com>
2013-02-18 04:40:29 -0800

    add firewall rules

--- cookbooks/server/attributes/default.rb
+++ cookbooks/server/attributes/default.rb
@@ -1,2 +1,9 @@
 override["jetty"]["java_options"] = "-Xmx384m -Djava.awt.headless=true -DJENKINS_HOME=/home/jetty/"
 override['nginx']['default_site_enabled'] = false
+override['firewall']['rules'] = [
+  {"http" => { "port" => "80" } },
+  {"https" => { "port" => "443" } },
+  {"jetty" => { "port" => "8080" } },
+  {"ssh" => { "port" => "22" } },
+  {"foresee" => { "port" => "3000" } }
+]
